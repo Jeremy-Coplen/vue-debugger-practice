e15b4beffc344352fa877c6c520bb50d2425c42a
Jeremy-Coplen <jeremycoplen.jc@gmail.com>
2022-05-02 03:24:48 -0600

    Finished project

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,114 @@
 
 <template>
   <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <header class="app-header">
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <h1>Simple Debugger</h1>
+    <nav>
+      <RouterLink class="nav-btn" active-class="nav-active" to="/">Home</RouterLink>
+      <RouterLink class="nav-btn" active-class="nav-active" to="/about">About</RouterLink>
+    </nav>
+  </header>
   </header>
 
   <RouterView />
 </template>
 
 <style>
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed, 
+  figure, figcaption, footer, header, hgroup, 
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  *, *::before, *::after {
+    -webkit-box-sizing: border-box;
+      -moz-box-sizing: border-box;
+      box-sizing: border-box;
+  }
+
+  .app-header {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    width: 100vw;
+  }
+
+  nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 50px;
+    text-decoration: none;
+    margin-bottom: 20px;
+  }
+
+  nav :first-child {
+    margin-right: 10px;
+  }
+
+  .nav-btn {
+    background-color: #40B280;
+    width: 75px;
+    text-align: center;
+    padding: 10px 0 10px 0;
+    border-radius: 10px;
+    text-decoration: none;
+    color: black;
+  }
+
+  .nav-btn:hover {
+    background-color: #126440;
+    cursor: pointer;
+  }
+
+  .nav-active {
+    background-color: #126440;
+  }
+
+  h1 {
+    font-weight: 700;
+    font-size: 30px;
+  }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,82 @@
 <script>
+    export default {
+        data() {
+            return {
+                count: 0,
+                message: "Nothing to report"
+            }
+        },
 
+        methods: {
+            decrement() {
+                console.log(this.message)
+                this.count--
+                this.message = "Counter value was decreased"
+                console.log(this.message)
+            },
+
+            increment() {
+                console.log(this.message)
+                this.count++
+                this.message="Counter value was increased"
+                console.log(this.message)
+            }
+        },
+    }
 </script>
 
 <template>
+    <div id="home">
+        <div id="counter-container">
+            <h1>Count:</h1>
+            <p id="number">{{ count }}</p>
+        </div>
+        <div id="button-container">
+            <button @click="decrement">Decrement</button>
+            <button @click="increment">Increment</button>
+        </div>
+    </div>
 </template>
+
+<style>
+    #home {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100vw;
+    }
+
+    #number {
+        font-size: 30px;
+    }
+
+    #home h1 {
+        margin-right: 5px;
+    }
+
+    #counter-container {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        width: 100vw;
+        margin-bottom: 10px;
+    }
+
+    #button-container  :first-child{
+        margin-right: 10px;
+    }
+
+    #button-container button {
+        width: 100px;
+        height: 30px;
+        background-color: #40B280;
+        border: none;
+        border-radius: 10px;
+    }
+
+    #button-container button:hover {
+        background-color: #126440;
+        cursor: pointer;
+    }
+</style>
